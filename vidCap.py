c61c78689a63495b26b1e345ad353d3f75b471d4
Noah Gray <njgray@calpoly.edu>
2019-02-02 10:48:00 -0800

    Cole

--- vidCap.py
+++ vidCap.py
@@ -1,18 +1,30 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture(1)
+
+cap = cv.VideoCapture(0)
+
+retval = cv.SimpleBlobDetector_create()
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame',frame)
-    k = cv.waitKey(1)
-    if k == ord('q'):
+    if ret == True:
+        # Detect blobs.
+        keypoints = retval.detect(frame)
+
+        # Draw detected blobs as red circles.
+        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+        im_with_keypoints = cv.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        # Display the resulting frame
+        cv.imshow('img', frame)
+        k =  cv.waitKey(1) & 0xFF
+        if k == ord('q'):
+            break
+        elif k == ord('c'):
+            cv.imwrite('capture.jpg', frame)
+    else:
         break
-    elif k == ord('c'):
-        cv.imwrite('capture.jpg', frame)
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
