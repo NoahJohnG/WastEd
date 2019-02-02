0974a8e59851f913ac26c786a564215d8919fa08
Noah Gray <njgray@calpoly.edu>
2019-02-02 10:58:00 -0800

    There ya go

--- vidCap.py
+++ vidCap.py
@@ -1,6 +1,11 @@
 import numpy as np
 import cv2 as cv
 
+def draw_keypoints(vis, keypoints, color = (0, 0, 255)):
+    for kp in keypoints:
+            x, y = kp.pt
+            cv.circle(vis, (int(x), int(y)), 2, color)
+
 cap = cv.VideoCapture(0)
 
 retval = cv.SimpleBlobDetector_create()
@@ -14,7 +19,7 @@ while(True):
 
         # Draw detected blobs as red circles.
         # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        im_with_keypoints = cv.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        im_with_keypoints = draw_keypoints(frame, keypoints, (0,0,255))
 
         # Display the resulting frame
         cv.imshow('img', frame)
