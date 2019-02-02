533145a693993060db981bb658f635c96f321c4d
Noah Gray <njgray@calpoly.edu>
2019-02-02 11:08:56 -0800

    oiajsdf

--- vidCap.py
+++ vidCap.py
@@ -4,9 +4,11 @@ import cv2 as cv
 def draw_keypoints(vis, keypoints, color = (0, 0, 255)):
     for kp in keypoints:
             x, y = kp.pt
-            cv.circle(vis, (int(x), int(y)), 2, color)
+            cv.circle(vis, (int(x), int(y)), int(kp.size/2), color)
 
 cap = cv.VideoCapture(0)
+
+fgbg = cv.createBackgroundSubtractorMOG2()
 
 retval = cv.SimpleBlobDetector_create()
 
@@ -14,15 +16,17 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     if ret == True:
+        # Apply Mask
+        fgmask = fgbg.apply(frame)
+
         # Detect blobs.
-        keypoints = retval.detect(frame)
+        keypoints = retval.detect(fgmask)
 
         # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        im_with_keypoints = draw_keypoints(frame, keypoints, (0,0,255))
+        draw_keypoints(fgmask, keypoints, (0,0,255))
 
         # Display the resulting frame
-        cv.imshow('img', frame)
+        cv.imshow('img', fgmask)
         k =  cv.waitKey(1) & 0xFF
         if k == ord('q'):
             break
