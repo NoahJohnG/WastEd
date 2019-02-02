e40508cf51cb4e76930c97ae6e4fbc996870c629
Noah Gray <njgray@calpoly.edu>
2019-02-02 14:29:40 -0800

    Staged pics

--- vidCap.py
+++ vidCap.py
@@ -14,10 +14,10 @@ ret,frame = cap.read()
 
 fgbg = cv.createBackgroundSubtractorMOG2()
 # Setup fbgb paramaters
-fgbg.setVarThreshold(300)
+fgbg.setVarThreshold(200)
 
 # setup initial location of window
-r,h,c,w = 250,90,400,125  # simply hardcoded the values
+r,h,c,w = 250,int(cap.get(4)/1.75),400,int(cap.get(3)/1.75) # simply hardcoded the values
 track_window = (c,r,w,h)
 # set up the ROI for tracking
 roi = frame[r:r+h, c:c+w]
@@ -29,21 +29,14 @@ cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
 # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
 term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
 
-frame_cnt = 0
-frame_limit = 10
-prev_pos = track_window[:2]
+stage = (int(cap.get(3)/2), int(cap.get(4)/2))
 dist = 0
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    #Inc frame counter
-    frame_cnt += 1
-    if frame_cnt == frame_limit:
-        dist = math.sqrt((prev_pos[0]-track_window[0])**2 + (prev_pos[1] - track_window[1])**2)
-        prev_pos = track_window[:2]
-        frame_cnt = 0
+    dist = math.sqrt((stage[0]-int(track_window[0] + track_window[2]/2))**2 + (stage[1] - int(track_window[1] + track_window[3]/2))**2)
 
     if ret == True:
         # Apply Mask
@@ -52,22 +45,24 @@ while(True):
         # apply meanshift to get the new location
         ret, track_window = cv.meanShift(fgmask, track_window, term_crit)
 
-        if dist > 100:
-            # Draw it on image
-            x,y,w,h = track_window
-            img2 = cv.rectangle(fgmask, (x,y), (x+w,y+h), 255,2)
-        else:
-            img2 = fgmask
+        if dist < 100:
+            # Output frame after waiting half a second
+            cv.waitKey(1500)
+            cv.imwrite('THING.JPG', frame)
+            break
+        x,y,w,h = track_window
+        center = (int(x + w/2), int(y + h/2))
+        img2 = cv.circle(fgmask, stage, 50, 100, 2)
+        img2 = cv.circle(img2, center, 5, 255,2)
+        cv.imshow("Empty", fgmask)
 
-        #Display image
-        cv.imshow('img2',img2)
         k =  cv.waitKey(1) & 0xFF
         if k == ord('q'):
             break
-        elif k == ord('c'):
-            cv.imwrite('capture.jpg', frame)
     else:
         break
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
+while cv.waitKey(1) & 0xFF != ord('q'):
+    cv.imshow('MOVEMENT', frame)
