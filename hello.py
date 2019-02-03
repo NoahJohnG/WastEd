00af63c4001bbc9cb83de6d5b3e650653a448d82
Noah Gray <njgray@calpoly.edu>
2019-02-03 03:52:17 -0800

    functioning capture

--- hello.py
+++ hello.py
@@ -104,9 +104,6 @@ def main():
 		write_to_arduino(trash_val, ard)
 		update_in_transaction(transaction, users_ref, 3)
 
-
-
-
 def int_to_byte(x):
     if(x ==1):
         return b'1'
@@ -124,54 +121,33 @@ def write_to_arduino(x, ard):
 	print(msg)
 
 def wait_trash():
-	cap = cv.VideoCapture(0)
+	cap = cv.VideoCapture(1)
 
-	fgbg = cv.createBackgroundSubtractorMOG2()
-	# Setup fbgb paramaters
-	fgbg.setVarThreshold(200)
+	with open('calibration.json') as f:
+		params = json.load(f)
 
-	# Setup initial location of window
-	r,h,c,w = 250,int(cap.get(4)/1.75),400,int(cap.get(3)/1.75) # simply hardcoded the values
-	track_window = (c,r,w,h)
-
-	# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-	term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
-
-	# Setup stage
-	stage = (int(cap.get(3)/2), int(cap.get(4)/2))
-	dist = 0
-
-	while(True):
-		# Capture frame-by-frame
-		ret, frame = cap.read()
-
-		dist = math.sqrt((stage[0]-int(track_window[0] + track_window[2]/2))**2 + (stage[1] - int(track_window[1] + track_window[3]/2))**2)
-
+	while True:
+		ret, img = cap.read()
+		
 		if ret == True:
-			# Apply Mask
-			fgmask = fgbg.apply(frame)
+			crop_img = img[params["y2"]:params["y1"], params["x1"]:params["x2"]]
+			hsv_crop = cv.cvtColor(crop_img, cv.COLOR_BGR2HSV)
+			thresh_hsv = cv.inRange(hsv_crop, (params["h_low"], params["s_low"], params["v_low"]), (params["h_high"], params["s_high"], params["v_high"]))
+			inv_thresh = 255 - thresh_hsv
 
-			# apply meanshift to get the new location
-			ret, track_window = cv.meanShift(fgmask, track_window, term_crit)
+			#Count number of white pixels
+			size = inv_thresh.size
+			count = cv.countNonZero(inv_thresh)
 
-			if dist < 100:
-				# Output frame after waiting a second and a half
-				cv.waitKey(1500)
-				cv.imwrite('THING.jpg', frame)
-				break
-			x,y,w,h = track_window
-			center = (int(x + w/2), int(y + h/2))
-			img2 = cv.circle(fgmask, stage, 50, 100, 2)
-			img2 = cv.circle(img2, center, 5, 255,2)
-			cv.imshow('waiting', img2)
-
-			k =  cv.waitKey(1) & 0xFF
-			if k == ord('q'):
+#			cv.imshow("blak", inv_thresh)
+			k = cv.waitKey(1) & 0xFF
+			if k == ord('q') or count > (size / 4):
+				cv.imwrite('THING.jpg', crop_img)
 				break
 		else:
 			break
-	# When everything done, release the capture
 	cap.release()
+	cv.destroyAllWindows()
 
 
 
