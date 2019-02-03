b167f5cd00bddc845ca6ee5e84a399403dc59b1c
Checkout User <checkoutuser@is-ct-a014505.local>
2019-02-02 21:41:10 -0800

    added motion sensor polling. need to remove arduino writing other info

--- hello.py
+++ hello.py
@@ -14,56 +14,53 @@ def main():
 	# Instantiates a client
 	client = vision.ImageAnnotatorClient()
 
-	wait_trash()
-
-	# The name of the image file to annotate
-	file_name = os.path.join(
-		os.path.dirname(__file__),'THING.jpg')
-
-	ard = serial.Serial("/dev/cu.usbmodem1411", 9600, timeout=2)
+		ard = serial.Serial("/dev/cu.usbmodem1411", 9600, timeout=2)
 	time.sleep(2)
 
-	with io.open("recycle.txt", "r") as recycle_file:
-		recycle_list = recycle_file.readlines()
-	with io.open("compost.txt", "r") as compost_file:
-		compost_list = compost_file.readlines()
+	while True:
+		wait_trash()
 
-	# Loads the image into memory
-	with io.open(file_name, 'rb') as image_file:
-		content = image_file.read()
+		# The name of the image file to annotate
+		file_name = os.path.join(
+			os.path.dirname(__file__),'THING.jpg')
 
-	image = types.Image(content=content)
+		with io.open("recycle.txt", "r") as recycle_file:
+			recycle_list = recycle_file.readlines()
+		with io.open("compost.txt", "r") as compost_file:
+			compost_list = compost_file.readlines()
 
-	# Performs label detection on the image file
-	response = client.label_detection(image=image)
-	labels = response.label_annotations
+		# Loads the image into memory
+		with io.open(file_name, 'rb') as image_file:
+			content = image_file.read()
 
-	# CONFIDENCE VALUE, UPDATE IF NECESSARY
+		image = types.Image(content=content)
 
-	labels = [x for x in labels if x.score > .5]
+		# Performs label detection on the image file
+		response = client.label_detection(image=image)
+		labels = response.label_annotations
 
-	if len(labels) == 0:
-		print("Unable to determine item!")
-		quit()
+		# CONFIDENCE VALUE, UPDATE IF NECESSARY
 
-	trash = True
-	for item in labels:
-		if (item.description + "\n") in recycle_list:
-			print("Recyclable!") # recycle 2
-			recycle_val = int_to_byte(2)
-			write_to_arduino(recycle_val, ard)
-			trash = False
-			break
-		elif (item.description + "\n") in compost_list:
-			print("Compostable!") # compost 1
-			compost_val = int_to_byte(1)
-			write_to_arduino(compost_val, ard)
-			trash = False
-			break
-	if trash:
-		print("Trash!") # trash 3
-		trash_val = int_to_byte(1)
-		write_to_arduino(trash_val, ard)
+		labels = [x for x in labels if x.score > .5]
+
+		trash = True
+		for item in labels:
+			if (item.description + "\n") in recycle_list:
+				print("Recyclable!") # recycle 2
+				recycle_val = int_to_byte(2)
+				write_to_arduino(recycle_val, ard)
+				trash = False
+				break
+			elif (item.description + "\n") in compost_list:
+				print("Compostable!") # compost 1
+				compost_val = int_to_byte(1)
+				write_to_arduino(compost_val, ard)
+				trash = False
+				break
+		if trash:
+			print("Trash!") # trash 3
+			trash_val = int_to_byte(1)
+			write_to_arduino(trash_val, ard)
 
 
 
@@ -87,7 +84,9 @@ def wait_trash():
 	cap = cv.VideoCapture(0)
 
 	while(True):
-		if 
+		dist = ard.readline()
+		if dist < threshhold:
+			break
 
 	# Capture frame-by-frame
 	ret, frame = cap.read()
